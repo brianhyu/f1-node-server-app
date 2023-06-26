a6cc4cfba806ab01404f1437596194e429aac470
fabigazi <fgaziano98@gmail.com>
2023-06-26 13:26:21 -0400

    test

--- speeds/speeds-model.js
+++ speeds/speeds-model.js
@@ -1,4 +1,4 @@
 import mongoose from "mongoose";
 import speedsSchema from "./speeds-schema.js";
-const speedsModel = mongoose.model("tuits", speedsSchema);
+const speedsModel = mongoose.model("speeds", speedsSchema);
 export default speedsModel;
