12405ad6b598611becab5d61b64b88754a23fc35
fabigazi <fgaziano98@gmail.com>
2023-06-25 21:13:06 -0400

    not sure why it makes me do that

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import session from "express-session";
 import mongoose from "mongoose";
 
 mongoose.connect(
-  "mongodb+srv://Guest:Guest@cluster0.kcmgz9i.mongodb.net/?retryWrites=true&w=majority"
+  "mongodb+srv://jannunzi:M3z9JRSvzhbUdSiu@cluster0.erbnm4p.mongodb.net/tuiter-su1-23?retryWrites=true&w=majority"
 );
 // mongoose.connect("mongodb://127.0.0.1:27017/tuiter-su1-23");
 
@@ -16,7 +16,6 @@ app.set("trust proxy", 1);
 app.use(
   cors({
     credentials: true,
-
     origin: "https://a6--resonant-quokka-5a61c8.netlify.app",
     // origin: "http://localhost:3000",
   })
